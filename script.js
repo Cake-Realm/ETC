ce0c2da6670e838419a36c158041054f9b6daff9
Darryl <darryl.dolan@hotmail.co.uk>
2017-10-05 11:28:03 +0200

    Version 1.1.0

--- script.js
+++ script.js
@@ -6,35 +6,44 @@ $(document).ready(function() {
   var active4 = false;
   var active5 = false;
 
-    $('.mask2').on('mousedown touchstart', function() {
+    $('.main-nav').on('mousedown touchstart', function() {
     // Magic code
+    // $().toggleClass();
     // Check current menu state, then changes CSS.
     if (!active1) {
-      $('.main-nav').find('.test1').css({'background-color': 'gray', 'transform': 'translate(100px,0px)'});
+      $(this).find('.test1').css({'transform': 'translate(100px,0px)'});
       // Make link text appear
-      $('.main-nav').find('.nav-link').css({'opacity': '0.5'});
-    }
-    else {
-      $('.main-nav').find('.test1').css({'background-color': 'silver', 'transform': 'none'});
-      $('.main-nav').find('.nav-link').css({'opacity': '0'});
+      $(this).find('.nav-link').css({'opacity': '0.5'});
+    } else {
+      $(this).find('.test1').css({'transform': 'none'});
+      $(this).find('.nav-link').css({'opacity': '0'});
     }
     // Education
-     if (!active2) $('.main-nav').find('.test2').css({'background-color': 'gray', 'transform': 'translate(200px,0px)'});
-    else $('.main-nav').find('.test2').css({'background-color': 'silver', 'transform': 'none'});
+    if (!active2) {
+      $(this).find('.test2').css({'transform': 'translate(200px,0px)'});
+      } else {
+      $(this).find('.test2').css({'transform': 'none'});
+    };
     // Code
-      if (!active3) $('.main-nav').find('.test3').css({'background-color': 'gray', 'transform': 'translate(300px,0px)'});
-    else $('.main-nav').find('.test3').css({'background-color': 'silver', 'transform': 'none'});
+    if (!active3) {
+      $(this).find('.test3').css({'transform': 'translate(300px,0px)'});
+    } else {
+      $(this).find('.test3').css({'transform': 'none'});
+    };
     // Message
-      if (!active4) $('.main-nav').find('.test4').css({'background-color': 'gray', 'transform': 'translate(400px,0px)'});
-    else $('.main-nav').find('.test4').css({'background-color': 'silver', 'transform': 'none'});
+    if (!active4) {
+      $(this).find('.test4').css({'transform': 'translate(400px,0px)'});
+    } else {
+      $(this).find('.test4').css({'transform': 'none'});
+    };
     // Gear
     if (!active5) {
-      $('.main-nav').find('.test5').css({'background-color': 'gray', 'transform': 'translate(500px,0px)'});
-    }
-    else {
-      $('.main-nav').find('.test5').css({'background-color': 'silver', 'transform': 'none'});
+      $(this).find('.test5').css({'transform': 'translate(500px,0px)'});
+    } else {
+      $(this).find('.test5').css({'transform': 'none'});
     }
     // Toggle
+    console.log('Reverting menu state...');
     active1 = !active1;
     active2 = !active2;
     active3 = !active3;
